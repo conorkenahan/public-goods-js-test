a55dfad3dc193e923c74f5557fc407246fb89d43
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-11-16 10:31:51 -0500

    initial commit

---
+++ public-goods-popup-test/script.js
@@ -0,0 +1,19 @@
+document.getElementById("popupBox").animate(
+  [
+    // keyframes
+    { transform: "translateY(1000px)" },
+    { transform: "translateY(-300px)" },
+  ],
+  {
+    // timing options
+    duration: 1000,
+  }
+);
+function openPopup() {
+  element = document.getElementById("popupBox");
+  element.style.visibility = "visible";
+}
+
+window.onload = function () {
+  setTimeout(openPopup, 10);
+};
