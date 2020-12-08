e5d7b062d04ceb4833aeebba0e44107a1d6a91b5
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-12-08 18:08:55 -0500

    position of tasks now persists when dragged

---
+++ dndtest/src/components/Column/Column.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+import Task from "../Task/Task";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+export default class Column extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {(provided) => (
+            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+              {this.props.tasks.map((task, i) => (
+                <Task key={task.id} task={task} index={i} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
+  }
+}
